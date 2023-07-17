28868dcda05cd0b8e00018683f70ac659b54477a
afeez1131 <lawalafeez052@gmail.com>
2023-07-17 07:46:48 +0100

    changes to API

--- api/serializers.py
+++ api/serializers.py
@@ -10,20 +10,33 @@ class FoodSerializers(serializers.ModelSerializer):
 
 
 class GeneratedFoodSerializers(serializers.Serializer):
-    breakfast = FoodSerializers()
-    lunch = FoodSerializers()
-    dinner = FoodSerializers()
+    breakfast = FoodSerializers(allow_null=True)
+    lunch = FoodSerializers(allow_null=True)
+    dinner = FoodSerializers(allow_null=True)
+
+    def to_representation(self, instance):
+        data = super(GeneratedFoodSerializers, self).to_representation(instance)
+        data['breakfast'] = data['breakfast'] or {}
+        data['lunch'] = data['lunch'] or {}
+        data['dinner'] = data['dinner'] or {}
+        return data
 
 
-class DaysSerializers(serializers.Serializer):
+class GetFoodSerializers(serializers.Serializer):
     no_of_days = serializers.IntegerField()
+    meal = serializers.CharField(allow_blank=True)
 
     def validate_no_of_days(self, value):
         # no_of_days = attrs.get('no_of_days')
-        print('in validate: ', value)
         if int(value) > 50:
             raise serializers.ValidationError('You cannot query more than 50 days')
         return value
+
+    def validate_meal(self, value):
+        if value and value.lower() not in ['breakfast', 'lunch', 'dinner']:
+            raise serializers.ValidationError('Not a valid meal type. should be one of "Breakfast", "Lunch", "Dinner"')
+        return value
+
 
 class RandomFoodSerializer(serializers.ModelSerializer):
     class Meta:
--- api/views.py
+++ api/views.py
@@ -14,6 +14,7 @@ class RandomFood(ListAPIView):
     """
     API Endpoint: GET /api/random
         Request:
+        Description: returns random Food and Description.
         Method: GET
         Response (Success):
             Status: 200 OK
@@ -35,8 +36,12 @@ class FoodList(CreateAPIView):
     """
     API Endpoint: POST /api/food
         Request:
+        Description: returns the Breakfast, Lunch and Dinner for the number of
+        days provided in the body.
+        If meal is provided, returns only food for the provided meal.
         Method: POST
-        Body: {"no_of_days": <number_of_days>}
+        Body: {"no_of_days": <number_of_days: int>,
+                "meal": <[breakfast|lunch|dinner]>}
         Response (Success):
             Status: 200 OK
             Body: {"status": "success", "data": [<food_list>]}
@@ -44,42 +49,55 @@ class FoodList(CreateAPIView):
             Status: 400 Bad Request
             Body: {"status": "error", "errors": <serializer_errors>}
     """
-    serializer_class = serializers.DaysSerializers
+    serializer_class = serializers.GetFoodSerializers
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             count = serializer.validated_data.get('no_of_days')
-            food_list = generate_food(count)
+            meal = serializer.validated_data.get('meal')
+            food_list = generate_food(count, meal)
             data = serializers.GeneratedFoodSerializers(food_list, many=True)
             return Response({'status': status.HTTP_200_OK, 'data': data.data})
         else:
             return Response({'status': status.HTTP_400_BAD_REQUEST, 'errors': serializer.errors})
 
 
-def generate_food(count):
+def generate_food(count: int, meal: str):
     foods = Food.objects.all()
-    breakfast = foods.filter(category=FoodCategoryChoices.BREAKFAST)
-    lunch = foods.filter(category=FoodCategoryChoices.LUNCH)
-    dinner = foods.filter(category=FoodCategoryChoices.DINNER)
+    meal_list = []
+    breakfast_list = []
+    lunch_list = []
+    dinner_list = []
+    if not meal:
+        breakfast = foods.filter(category=FoodCategoryChoices.BREAKFAST)
+        lunch = foods.filter(category=FoodCategoryChoices.LUNCH)
+        dinner = foods.filter(category=FoodCategoryChoices.DINNER)
 
-    lunch_list = list(lunch)
-    breakfast_list = list(breakfast)
-    dinner_list = list(dinner)
+        lunch_list = list(lunch)
+        breakfast_list = list(breakfast)
+        dinner_list = list(dinner)
+    else:
+        meal_list = list(foods.filter(category=meal))
+
     food_list = []
     for i in range(count):
-        random.shuffle(breakfast_list)
-        random.shuffle(lunch_list)
-        random.shuffle(dinner_list)
-
         food_dict = dict()
-        breakfast = random.choice(breakfast_list)
-        lunch = random.choice(lunch_list)
-        dinner = random.choice(dinner_list)
+        if not meal:
+            random.shuffle(breakfast_list)
+            random.shuffle(lunch_list)
+            random.shuffle(dinner_list)
 
-        food_dict['breakfast'] = breakfast
-        food_dict['lunch'] = lunch
-        food_dict['dinner'] = dinner
+            breakfast = random.choice(breakfast_list)
+            lunch = random.choice(lunch_list)
+            dinner = random.choice(dinner_list)
+
+            food_dict['breakfast'] = breakfast
+            food_dict['lunch'] = lunch
+            food_dict['dinner'] = dinner
+        else:
+            random.shuffle(meal_list)
+            food_dict[meal] = random.choice(meal_list)
 
         food_list.append(food_dict)
     return food_list
