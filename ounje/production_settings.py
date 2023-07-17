177f4bc83ec3c4a7872d43c003ac026e01876b59
afeez1131 <lawalafeez052@gmail.com>
2023-07-17 22:26:37 +0100

    changes

--- ounje/production_settings.py
+++ ounje/production_settings.py
@@ -1,9 +1,11 @@
+import os
+
 import dj_database_url
 from decouple import config
 
 from ounje.settings import BASE_DIR
 
 DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600),
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
--- ounje/settings.py
+++ ounje/settings.py
@@ -48,12 +48,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ounje.urls'
@@ -161,6 +163,8 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 is_Live = True
 
